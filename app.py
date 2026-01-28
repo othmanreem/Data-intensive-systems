21498776dd137eb6a030bbf54d8e8d774c59cba4
Wagtail <Bachstelze@users.noreply.github.com>
2026-01-28 15:12:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import gradio as gr
 
+similar_app = "https://huggingface.co/spaces/sems/pose-think"
+
 def greet(name):
     return "Hello " + name + "!!"
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
+description_string = """
+Simple Demo app that should be developed into a Movement Assessment similar to the [posture analysis](https://huggingface.co/spaces/sems/pose-think).
+"""
+iface = gr.Interface(fn=greet, inputs="text", outputs="text", description=description_string)
 iface.launch()
